6bb5bf092e55f18019c85e1053fbbb4335aa9974
Oussama Mos <oussamabader@outlook.com>
2021-12-13 12:22:59 +0100

    Implémentation du 1ère méthode correctement avec Spring Boot

--- src/main/java/com/miniprojet/miniprojet/Repository/CompteRepository.java
+++ src/main/java/com/miniprojet/miniprojet/Repository/CompteRepository.java
@@ -1,9 +1,14 @@
 package com.miniprojet.miniprojet.Repository;
 
+import java.util.List;
+
 import com.miniprojet.miniprojet.Model.Compte;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CompteRepository extends CrudRepository<Compte, Integer>{
-    
+    @Query("from Compte where mail = :mail and password = :password")
+    List<Compte> connecterAvecMail(@Param("mail") String mail, @Param("password") String password);
 }
